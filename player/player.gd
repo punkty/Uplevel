7111b8a99e6ca59605eac02be56573d8d04a5d4a
ty <iamtylerwarren@gmail.com>
2024-04-08 20:28:38 -0700

    Add godot project

---
+++ player/player.gd
@@ -0,0 +1,29 @@
+extends CharacterBody2D
+
+@export var speed: int = 80
+@onready var animations = $AnimationPlayer
+
+func handleInput():
+	var moveDirection = Input.get_vector("ui_left","ui_right","ui_up","ui_down")
+	velocity = moveDirection*speed
+	
+
+func updateAnimation():
+	if velocity.length() == 0:
+		
+		animations.stop()
+	else:	
+		var direction = "Down"
+		if (velocity.x < 0 ):
+			direction = "Left"
+		elif (velocity.x > 0):
+			direction = "Right"
+		elif (velocity.y < 0):
+			direction = "Up"
+		animations.play("walk" + direction)	
+	
+#built-in callback function and is called each physics step
+func _physics_process(delta):
+	handleInput()
+	move_and_collide(delta * velocity)
+	updateAnimation()
